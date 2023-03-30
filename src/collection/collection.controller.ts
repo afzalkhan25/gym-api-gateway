096877993d52693e4c1856aba2ab7b690b7c0db2
chandannaresh <chandansutradhar2@gmail.com>
2023-03-30 10:08:37 +0530

    stream reader demo

--- src/collection/collection.controller.ts
+++ src/collection/collection.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, StreamableFile } from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 import { CollectionService } from './collection.service';
 import { CreateCollectionDto } from './dto/create-collection.dto';
 import { UpdateCollectionDto } from './dto/update-collection.dto';
 
 @Controller('collection')
 export class CollectionController {
-  constructor(private readonly collectionService: CollectionService) {}
+  constructor(private readonly collectionService: CollectionService) { }
 
   @Post()
   create(@Body() createCollectionDto: CreateCollectionDto) {
@@ -30,5 +32,21 @@ export class CollectionController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.collectionService.remove(+id);
+  }
+
+
+  // @Get('/invoice/:id')
+  // getInvoice(@Res() res: Response) {
+  //   const file = createReadStream(join(process.cwd(), 'package.json'));
+  //   //file.pipe(res);
+  //   return new StreamableFile(file);
+  // }
+
+  @Get('/invoice/:id')
+  getInvoice(@Res() res: Response): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'package.json'));
+    //file.pipe(res);
+
+    return new StreamableFile(file);
   }
 }
